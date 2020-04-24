7fca95c68b15a06de1bc7a840215a79ad01d48f1
Kevin GUILLARD <stolcraft@gmail.com>
2020-04-24 15:55:00 +0200

    save personne

---
+++ GestionScolaireSpringBoot-master/src/main/java/eu/ensup/gestionscolairespringboot/dao/PersonneRepository.java
@@ -0,0 +1,10 @@
+package eu.ensup.gestionscolairespringboot.dao;
+
+
+import eu.ensup.gestionscolairespringboot.domaine.Personne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonneRepository extends JpaRepository<Personne, Integer>{
+
+	public Personne save(Personne personne);
+}
