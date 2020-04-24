43b5a646d1e0786562619e6dc71d74e85e0c0bf1
Kevin GUILLARD <stolcraft@gmail.com>
2020-04-24 17:52:08 +0200

    le push final

--- GestionScolaireSpringBoot-master/src/main/resources/data.sql
+++ GestionScolaireSpringBoot-master/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `personne` (`id`, `adresse`, `mail`, `nom`, `prenom`, `telephone`) VALUES
-(1, '1 avenue de Paris', 'gtm@ensup.eu', 'Admin', 'admin', 12345678);
+(60, '1 avenue de Paris', 'gtm@ensup.eu', 'Admin', 'admin', 12345678);
 
 
 
 INSERT INTO `direction` (`login`, `password`, `id`) VALUES
-('root', 'root', 1);
+('root', 'root', 60);
