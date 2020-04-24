90fb0171e4856f255453019d579597394df12620
Cyril Gautray <gautray.cyril@hotmail.com>
2020-04-24 16:56:15 +0200

    test Dao 1 create

---
+++ GestionScolaireSpringBoot-master/src/test/java/eu/ensup/gestionscolairespringboot/daotest/DaoTest.java
@@ -0,0 +1,45 @@
+package eu.ensup.gestionscolairespringboot.daotest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import eu.ensup.gestionscolairespringboot.dao.EtudiantRepository;
+import eu.ensup.gestionscolairespringboot.domaine.Etudiant;
+
+@SpringBootTest
+class DaoTest {
+	// test de la couche DAO avec des stub
+
+	@Autowired
+	private EtudiantRepository ietudiantdao;
+
+	@Test
+	void createShouldReturnTrue() {
+		// insert un etudiant en bdd dont le prénom est "test" et asserte que le prenom
+		// de l'étudiant correspond à la valeur "test"
+		Etudiant etu = new Etudiant();
+		etu.setPrenom("test");
+		assertThat(ietudiantdao.save(etu).getPrenom()).isEqualTo("test");
+	}
+
+	@Test
+	void readShouldReturnTrue() {
+
+		fail();
+	}
+
+	@Test
+	void updateShouldReturnTrue() {
+		fail();
+	}
+
+	@Test
+	void deleteShouldReturnTrue() {
+		fail();
+	}
+
+}
