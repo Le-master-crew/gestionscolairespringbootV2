cddfd9393f6b22583eb84e7146d7ef8c49bc8302
BenKapoor <benjaminkapoor@yahoo.fr>
2020-05-14 10:55:11 +0200

    v2 controller error handler

--- GestionScolaireSpringBoot-master/src/main/java/eu/ensup/gestionscolairespringboot/controller/HandlerErrorController.java
+++ GestionScolaireSpringBoot-master/src/main/java/eu/ensup/gestionscolairespringboot/controller/HandlerErrorController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyErrorController implements ErrorController  {
+public class HandlerErrorController implements ErrorController  {
     
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
