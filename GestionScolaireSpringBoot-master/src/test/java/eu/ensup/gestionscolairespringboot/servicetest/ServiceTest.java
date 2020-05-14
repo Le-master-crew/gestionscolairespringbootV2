c352b9759a81bbc9f90c81af97f2542ad81eadb2
Khady NDIAYE <diijandiaye6@gmail.com>
2020-05-14 09:52:25 +0200

    Init Service Test

---
+++ GestionScolaireSpringBoot-master/src/test/java/eu/ensup/gestionscolairespringboot/servicetest/ServiceTest.java
@@ -0,0 +1,14 @@
+package eu.ensup.gestionscolairespringboot.servicetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
