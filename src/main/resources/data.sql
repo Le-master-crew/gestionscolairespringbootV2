fab9bdedef7c893249ddfa6bd6c7d63e64266079
Kevin GUILLARD <stolcraft@gmail.com>
2020-05-15 10:38:13 +0200

    heroku

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
-INSERT INTO `personne` (`id`, `adresse`, `mail`, `nom`, `prenom`, `telephone`) VALUES
+INSERT INTO personne (id, adresse, mail, nom, prenom, telephone) VALUES
 (60, '1 avenue de Paris', 'gtm@ensup.eu', 'Admin', 'admin', 12345678);
 
 
-INSERT INTO `direction` (`login`, `password`, `id`) VALUES
+
+INSERT INTO direction (login, password, id) VALUES
 ('root', 'root', 60);
